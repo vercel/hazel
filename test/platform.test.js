008958c9eba11a31b1909a37d244153df8514eff
Tim Neutkens <timneutkens@me.com>
2017-11-20 06:50:58 +0100

    Add tests

---
+++ test/platform.test.js
@@ -0,0 +1,17 @@
+/* global describe, it, expect */
+const platform = require('../lib/platform')
+
+describe('Platform', () => {
+    it('Should parse mac', () => {
+        const result = platform('hyper-2.1.1-mac.zip')
+        expect(result).toBe('darwin')
+    })
+    it('Should parse other platforms', () => {
+        const result = platform('hyper_2.1.1_amd64.deb')
+        expect(result).toBe('deb')      
+    })
+    it('Should return false for unknown files', () => {
+        const result = platform('hi.txt')
+        expect(result).toBe(false)
+    })
+})
