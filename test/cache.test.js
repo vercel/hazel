96742b2fb3f0a542a303cc9999c8d4e766e5fdc1
Tim Neutkens <tim@timneutkens.nl>
2017-11-21 16:12:49 +0100

    Programmatic API added (#33)
    * Add programmatic api
    
    * Add tests
    
    * Lint
    
    * Remove timeout
    
    * Add docs
    
    * Run tests properly

---
+++ test/cache.test.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-new */
+/* global describe, it, expect */
+const Cache = require('../lib/cache')
+
+describe('Cache', () => {
+  it('should throw when account is not defined', () => {
+    expect(() => {
+      const config = { repository: 'hyper' }
+      new Cache(config)
+    }).toThrow(/ACCOUNT/)
+  })
+
+  it('should throw when repository is not defined', () => {
+    expect(() => {
+      const config = { account: 'zeit' }
+      new Cache(config)
+    }).toThrow(/REPOSITORY/)
+  })
+
+  it('should throw when token is defined and url is not', () => {
+    expect(() => {
+      const config = { account: 'zeit', repository: 'hyper', token: 'abc' }
+      new Cache(config)
+    }).toThrow(/URL/)
+  })
+
+  it('should run without errors', () => {
+    const config = { account: 'zeit', repository: 'hyper' }
+    new Cache(config)
+  })
+
+  it('should refresh the cache', async () => {
+    const config = { account: 'zeit', repository: 'hyper' }
+    const cache = new Cache(config)
+    await cache.refreshCache()
+    const storage = cache.loadCache()
+    expect(typeof storage.version).toBe('string')
+    expect(typeof storage.platforms).toBe('object')
+  })
+
+  it('should set platforms correctly', async () => {
+    const config = { account: 'zeit', repository: 'hyper' }
+    const cache = new Cache(config)
+    await cache.refreshCache()
+    const storage = cache.loadCache()
+    console.log(storage.platforms.darwin)
+  })
+})
