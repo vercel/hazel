cd91f357e6b999611e4b1ea6c0b61298dd7e9c5c
Leo Lamprecht <mindrun@icloud.com>
2020-02-23 23:20:22 +0100

    Added download endpoint

--- pages/download/[platform].js
+++ pages/download/[platform].js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import getLatest from '../../lib/get-latest';
 import { extensionAliases } from '../../lib/extensions';
 
@@ -34,12 +35,58 @@ export async function unstable_getStaticProps(context) {
   }
 
   return {
-    props: match && latest.platforms[match]
+    props: {
+      file: match && latest.platforms[match],
+      repository: latest.repository,
+      version: latest.version
+    }
   };
 };
 
 export default function(props) {
   return (
-    <meta httpEquiv="Refresh" content={`0; url=${props.downloadUrl}`} />
+    <main>
+      <Head>
+        <meta httpEquiv="Refresh" content={`5; url=${props.file.downloadUrl}`} />
+      </Head>
+
+      <style jsx global>{`
+        html,
+        body,
+        #__next {
+          height: 100%;
+          width: 100%;
+        }
+
+        body {
+          background: #000;
+          margin: 0;
+        }
+
+        #__next {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      `}</style>
+
+    <style jsx>{`
+      main {
+        color: #FFF;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+            'Oxygen', 'Ubuntu', 'Fira Sans', 'Helvetica Neue', sans-serif;
+      }
+
+      h1 {
+        font-weight: normal;
+      }
+
+      b {
+        border-bottom: 2px solid #fff;
+      }
+    `}</style>
+
+  <h1>Downloading <b>{props.repository} {props.version}</b></h1>
+    </main>
   );
 };
