ffca85be7f7071b3942b8b47df000dec0f156ee6
Leo Lamprecht <mindrun@icloud.com>
2020-02-23 22:00:56 +0100

    Made all the view data dynamic

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
+import Head from 'next/head';
+import bytes from 'bytes';
 import getLatest from '../lib/get-latest';
 
 export async function unstable_getStaticProps(context) {
   return {
-    props: await getLatest('zeit', 'hyper')
+    props: await getLatest()
   };
 }
 
 export default props => {
-  console.log(props);
+  const githubUrl = `https://github.com/${props.account}/${props.repository}/`;
+
   return (
     <main>
+      <Head>
+        <title>Download {props.repository}</title>
+      </Head>
+
       <style jsx global>{`
         html,
         body,
@@ -43,8 +50,8 @@ export default props => {
 
         main {
           color: #fff;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-            "Oxygen", "Ubuntu", "Fira Sans", "Helvetica Neue", sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+            'Oxygen', 'Ubuntu', 'Fira Sans', 'Helvetica Neue', sans-serif;
           max-width: 400px;
           width: 100%;
           padding: 0 24px;
@@ -87,44 +94,32 @@ export default props => {
 
       <aside>
         <h1>
-          zeit/<b>hyper</b>
+          {props.account}/<b>{props.repository}</b>
         </h1>
-        <time>10 months ago</time>
+        <time>{props.age}</time>
       </aside>
 
       <nav>
-        <span>
-          <a href="#">.dmg</a>
-          <i>53.8 MB</i>
-        </span>
-        <span>
-          <a href="#">.AppImage</a>
-          <i>57.1 MB</i>
-        </span>
-        <span>
-          <a href="#">.zip</a>
-          <i>55.6 MB</i>
-        </span>
-        <span>
-          <a href="#">.rpm</a>
-          <i>39.2 MB</i>
-        </span>
-        <span>
-          <a href="#">.exe</a>
-          <i>60 MB</i>
-        </span>
-        <span>
-          <a href="#">.deb</a>
-          <i>39.2 MB</i>
-        </span>
+        {Object.keys(props.platforms).map(extension => {
+          const platform = props.platforms[extension];
+
+          return (
+            <span>
+              <a href={platform.url}>{extension}</a>
+              <i>{bytes(platform.size, { unitSeparator: ' ' })}</i>
+            </span>
+          );
+        })}
       </nav>
 
       <aside>
-        <h2>3.0.2</h2>
+        <h2>{props.version}</h2>
 
         <legend>
-          <a href="#">Release Notes</a>
-          <a href="#">All Releases</a>
+          <a href={githubUrl + 'releases/tag/' + props.version}>
+            Release Notes
+          </a>
+          <a href={githubUrl + 'releases'}>All Releases</a>
         </legend>
       </aside>
     </main>
