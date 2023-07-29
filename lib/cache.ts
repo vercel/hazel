30c601e59edcd7442a318fe3f5657a761bf4699a
PascalPixel <x@pascalpixel.com>
2023-07-29 19:52:41 +0100

    add errors

--- lib/cache.ts
+++ lib/cache.ts
@@ -2,7 +2,7 @@ import retry from "async-retry";
 import fetch from "cross-fetch";
 import ms from "ms";
 
-import type { HazelConfig, HazelError } from "./index.js";
+import type { HazelConfig } from "./index.js";
 import { patchPlatform } from "./utils.js";
 
 type PlatformType = {
@@ -36,19 +36,13 @@ export class HazelCache {
     this.config = config;
 
     if (!account || !repository) {
-      const error = new Error(
-        "Neither ACCOUNT, nor REPOSITORY are defined",
-      ) as HazelError;
-      error.code = "missing_configuration_properties";
-      throw error;
+      throw new Error("Neither ACCOUNT, nor REPOSITORY are defined");
     }
 
     if (token && !url) {
-      const error = new Error(
+      throw new Error(
         "Neither VERCEL_URL, nor URL are defined, which are mandatory for private repo mode",
-      ) as HazelError;
-      error.code = "missing_configuration_properties";
-      throw error;
+      );
     }
 
     this.latest = {};
@@ -61,149 +55,131 @@ export class HazelCache {
   }
 
   async cacheReleaseList(url: string) {
-    try {
-      const { token } = this.config;
-      const headers: RequestInit["headers"] = {
-        Accept: "application/octet-stream",
-      };
+    const { token } = this.config;
+    const headers: RequestInit["headers"] = {
+      Accept: "application/octet-stream",
+    };
 
-      if (token && typeof token === "string" && token.length > 0) {
-        headers.Authorization = `token ${token}`;
-      }
-
-      const { body } = await retry(
-        async () => {
-          const response = await fetch(url, { headers });
-
-          if (response.status !== 200) {
-            throw new Error(
-              `Tried to cache RELEASES, but failed fetching ${url}, status ${response.status}`,
-            );
-          }
-
-          return response;
-        },
-        { retries: 3 },
-      );
-
-      const content = await convertStream(body);
-      const matches = content.match(/[^ ]*\.nupkg/gim);
-
-      if (!matches || matches.length === 0) {
-        throw new Error(
-          `Tried to cache RELEASES, but failed. RELEASES content doesn't contain nupkg`,
-        );
-      }
-
-      return content;
-    } catch (err) {
-      console.error(err);
-      return "";
+    if (token && typeof token === "string" && token.length > 0) {
+      headers.Authorization = `token ${token}`;
     }
+
+    const { body } = await retry(
+      async () => {
+        const response = await fetch(url, { headers });
+
+        if (response.status !== 200) {
+          throw new Error(
+            `Tried to cache RELEASES, but failed fetching ${url}, status ${response.status}`,
+          );
+        }
+
+        return response;
+      },
+      { retries: 3 },
+    );
+
+    const content = await convertStream(body);
+    const matches = content.match(/[^ ]*\.nupkg/gim);
+
+    if (!matches || matches.length === 0) {
+      throw new Error(
+        `Tried to cache RELEASES, but failed. RELEASES content doesn't contain nupkg`,
+      );
+    }
+
+    return content;
   }
 
   async refreshCache() {
-    try {
-      const { account, repository, pre, token } = this.config;
-      const repo = account + "/" + repository;
-      const url = `https://api.github.com/repos/${repo}/releases?per_page=100`;
-      const headers: RequestInit["headers"] = {
-        Accept: "application/vnd.github.preview",
-      };
+    const { account, repository, pre, token } = this.config;
+    const repo = account + "/" + repository;
+    const url = `https://api.github.com/repos/${repo}/releases?per_page=100`;
+    const headers: RequestInit["headers"] = {
+      Accept: "application/vnd.github.preview",
+    };
 
-      if (token && typeof token === "string" && token.length > 0) {
-        headers.Authorization = `token ${token}`;
-      }
-
-      const response = await retry(
-        async () => {
-          const response = await fetch(url, { headers });
-
-          if (response.status !== 200) {
-            throw new Error(
-              `GitHub API responded with ${response.status} for url ${url}`,
-            );
-          }
-
-          return response;
-        },
-        { retries: 3 },
-      );
-
-      const data = await response.json();
-
-      if (!Array.isArray(data) || data.length === 0) {
-        return;
-      }
-
-      const release = data.find((item) => {
-        const isPre = Boolean(pre) === Boolean(item.prerelease);
-        return !item.draft && isPre;
-      });
-
-      if (!release || !release.assets || !Array.isArray(release.assets)) {
-        return;
-      }
-
-      const { tag_name } = release;
-
-      if (this.latest.version === tag_name) {
-        console.log("Cached version is the same as latest");
-        this.lastUpdate = Date.now();
-        return;
-      }
-
-      console.log(`Caching version ${tag_name}...`);
-
-      this.latest.version = tag_name;
-      this.latest.notes = release.body;
-      this.latest.pub_date = release.published_at;
-
-      // Clear list of download links
-      this.latest.platforms = {};
-
-      for (const asset of release.assets) {
-        const { name, browser_download_url, url, content_type, size } = asset;
-
-        if (name === "RELEASES") {
-          try {
-            if (!this.latest.files) {
-              this.latest.files = {};
-            }
-            this.latest.files.RELEASES = await this.cacheReleaseList(url);
-          } catch (err) {
-            console.error(err);
-          }
-          continue;
-        }
-
-        const platform = patchPlatform(name);
-
-        if (!platform) {
-          continue;
-        }
-
-        this.latest.platforms[platform] = {
-          name,
-          api_url: url,
-          url: browser_download_url,
-          content_type,
-          size: Math.round((size / 1000000) * 10) / 10,
-        };
-      }
-
-      console.log(`Finished caching version ${tag_name}`);
-      this.lastUpdate = Date.now();
-    } catch (err) {
-      console.error(err);
-
-      const error = err as HazelError;
-      if (error.code === "missing_configuration_properties") {
-        throw err;
-      }
-
-      console.log("Failed to cache latest version");
+    if (token && typeof token === "string" && token.length > 0) {
+      headers.Authorization = `token ${token}`;
     }
+
+    const response = await retry(
+      async () => {
+        const response = await fetch(url, { headers });
+
+        if (response.status !== 200) {
+          throw new Error(
+            `GitHub API responded with ${response.status} for url ${url}`,
+          );
+        }
+
+        return response;
+      },
+      { retries: 3 },
+    );
+
+    const data = await response.json();
+
+    if (!Array.isArray(data) || data.length === 0) {
+      return;
+    }
+
+    const release = data.find((item) => {
+      const isPre = Boolean(pre) === Boolean(item.prerelease);
+      return !item.draft && isPre;
+    });
+
+    if (!release || !release.assets || !Array.isArray(release.assets)) {
+      return;
+    }
+
+    const { tag_name } = release;
+
+    if (this.latest.version === tag_name) {
+      console.log("Cached version is the same as latest");
+      this.lastUpdate = Date.now();
+      return;
+    }
+
+    console.log(`Caching version ${tag_name}...`);
+
+    this.latest.version = tag_name;
+    this.latest.notes = release.body;
+    this.latest.pub_date = release.published_at;
+
+    // Clear list of download links
+    this.latest.platforms = {};
+
+    for (const asset of release.assets) {
+      const { name, browser_download_url, url, content_type, size } = asset;
+
+      if (name === "RELEASES") {
+        try {
+          if (!this.latest.files) this.latest.files = {};
+          this.latest.files.RELEASES = await this.cacheReleaseList(url);
+        } catch (err) {
+          throw new Error("Failed to refresh RELEASES");
+        }
+        continue;
+      }
+
+      const platform = patchPlatform(name);
+
+      if (!platform) {
+        continue;
+      }
+
+      this.latest.platforms[platform] = {
+        name,
+        api_url: url,
+        url: browser_download_url,
+        content_type,
+        size: Math.round((size / 1000000) * 10) / 10,
+      };
+    }
+
+    console.log(`Finished caching version ${tag_name}`);
+    this.lastUpdate = Date.now();
   }
 
   isOutdated() {
@@ -217,8 +193,7 @@ export class HazelCache {
 
       return false;
     } catch (err) {
-      console.error(err);
-      return false;
+      throw new Error("Failed to check if cache is outdated");
     }
   }
 
@@ -237,8 +212,7 @@ export class HazelCache {
       console.log(storage);
       return storage;
     } catch (err) {
-      console.error(err);
-      return {};
+      throw new Error("Failed to load cache");
     }
   }
 }
